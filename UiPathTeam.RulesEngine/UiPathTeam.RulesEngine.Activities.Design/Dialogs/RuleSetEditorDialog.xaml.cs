ef8b621d67ca566872ea51dad85e474238ef3512
omar-saleh-01 <omar.saleh@uipath.com>
2022-09-29 20:08:19 +0400

    validate version textbox - only numbers

--- UiPathTeam.RulesEngine/UiPathTeam.RulesEngine.Activities.Design/Dialogs/RuleSetEditorDialog.xaml.cs
+++ UiPathTeam.RulesEngine/UiPathTeam.RulesEngine.Activities.Design/Dialogs/RuleSetEditorDialog.xaml.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -369,7 +370,7 @@ namespace UiPathTeam.RulesEngine.Activities.Design.Dialogs
 
         private void txtMajorVersion_LostFocus(object sender, RoutedEventArgs e)
         {
-            ///e.Cancel = false;
+            //e.Cancel = false;
             int majorInt = Convert.ToInt32(txtMajorVersion.Text);
             if (selectedRuleSetData != null && TreeRuleSets.SelectedItem != null && majorInt != selectedRuleSetData.MajorVersion)
             {
@@ -660,5 +661,40 @@ namespace UiPathTeam.RulesEngine.Activities.Design.Dialogs
             }
             return ruleSetDataCollection;
         }
+
+
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void txtMajorVersion_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void txtMinorVersion_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        // Use the DataObject.Pasting Handler 
+        private void TextBoxPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
     }
 }
